2913b3d7265d9d83af16ac8c0be4fd05caa4e87d
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2022-08-22 21:33:58 +0300

    Specify data types for `Matrix` class

--- wrapper.py
+++ wrapper.py
@@ -1,9 +1,32 @@
-import ctypes
+from ctypes import (
+    CDLL,
+    POINTER,
+    Structure,
+    c_double,
+    c_int,
+)
 
-lib = ctypes.CDLL('./lib.so')
+
+class Matrix(Structure):
+    _fields_ = [
+        ('rows', c_int),
+        ('columns', c_int),
+        ('data', POINTER(POINTER(c_double))),
+    ]
+
+
+lib = CDLL('./lib.so')
 
 main = lib.main
-main.restype = ctypes.c_int
+main.restype = c_int
+
+multiply = lib.multiply
+multiply.argtypes = [c_int, c_int]
+multiply.restype = Matrix
+
+a = Matrix(c_int(2), c_int(4))
+b = Matrix(c_int(4), c_int(1))
+c = multiply(a, b)
 
 print('Start')
 main()
