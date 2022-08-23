d606daf772a297e9e5f9d845eaa4621c0433eb52
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2022-08-24 02:35:44 +0300

    Create `Matrix` instances using arrays of `Vector`

--- wrapper.py
+++ wrapper.py
@@ -5,6 +5,9 @@ from ctypes import (
     c_double,
     c_int,
 )
+from typing import List
+
+from vector import make_vector_python, Vector
 
 
 class Matrix(Structure):
@@ -20,13 +23,44 @@ lib = CDLL('./lib.so')
 main = lib.main
 main.restype = c_int
 
+make_matrix = lib.make_matrix
+lib.make_matrix.argtypes = [c_int, POINTER(Vector)]
+lib.make_matrix.restype = Matrix
+
+print_matrix = lib.print_matrix
+print_matrix.argtypes = [Matrix]
+print_matrix.restype = None
+
 multiply = lib.multiply
-multiply.argtypes = [c_int, c_int]
+multiply.argtypes = [Matrix, Matrix]
 multiply.restype = Matrix
 
-a = Matrix(c_int(2), c_int(4))
-b = Matrix(c_int(4), c_int(1))
+
+def make_matrix_python(values: List[List[float]]):
+    vectors: List[Vector] = []
+    for row in values:
+        vectors.append(make_vector_python(row))
+    n_rows = len(values)
+    return make_matrix(n_rows, (Vector * n_rows)(*vectors))
+
+
+a = make_matrix_python([
+    [1, 2, 3],
+    [5, 3, 1]])
+
+b = make_matrix_python([
+    [8, 2],
+    [5, 3],
+    [6, 2]])
+
 c = multiply(a, b)
+
+print('Matrix A:')
+print_matrix(a)
+print('Matrix B:')
+print_matrix(b)
+print('Result:')
+print_matrix(c)
 
 print('Start')
 main()
