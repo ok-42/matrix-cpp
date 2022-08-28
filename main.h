525d6dc20e1a5f8e74b41dfb74c64f1342121724
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2022-08-28 13:07:48 +0300

    Add a C++ function to print a matrix

--- main.h
+++ main.h
@@ -20,5 +20,8 @@ extern "C" {
     Matrix make_matrix_orig(int rows, int columns, double** values);
     Matrix make_matrix(int rows, Vector* vectors);
     Matrix multiply(Matrix a, Matrix b);
+    void print_matrix(Matrix matrix) {
+        matrix.print();
+    }
     int main();
 }
