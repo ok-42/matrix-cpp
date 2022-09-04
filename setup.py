f745fb9c32e859aaaa78125b5b660455de354cad
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2022-09-04 23:09:56 +0300

    Create `setup.py` file

---
+++ setup.py
@@ -0,0 +1,50 @@
+# Magic from https://stackoverflow.com/a/34830639
+
+from setuptools import find_packages
+from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext as build_ext_orig
+
+
+class build_ext(build_ext_orig):
+
+    def build_extension(self, ext):
+        self._ctypes = isinstance(ext, CTypes)
+        return super().build_extension(ext)
+
+    def get_export_symbols(self, ext):
+        if self._ctypes:
+            return ext.export_symbols
+        return super().get_export_symbols(ext)
+
+    def get_ext_filename(self, ext_name):
+        if self._ctypes:
+            return ext_name + '.so'
+        return super().get_ext_filename(ext_name)
+
+
+class CTypes(Extension):
+    pass
+
+
+files = [
+    'cpp/main.cpp', 'cpp/vector.cpp',
+]
+
+setup(
+    name='mc',
+    version='1.0',
+    ext_modules=[
+        CTypes(
+            'ct',
+            sources=files,
+            include_dirs=['.']
+        ),
+        Extension(
+            'ext',
+            sources=files,
+            include_dirs=['.']
+        )
+    ],
+    cmdclass={'build_ext': build_ext},
+    packages=find_packages()
+)
