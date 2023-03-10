552fdeb77eabf45799156807b7cee6c4c76f139c
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2023-03-11 01:30:33 +0600

    Add a universal function to create both matrices and vectors

--- mc/__init__.py
+++ mc/__init__.py
@@ -1,2 +1,21 @@
-from .matrix import Matrix, make_matrix_python as make_matrix
+# from typing import List, Union
+
+from .matrix import Matrix, make_matrix_python as make_matrix, MatrixType
 from .vector import Vector, make_vector_python as make_vector
+
+
+# def make_mc(values: Union[MatrixType, List[float]]) -> Union[Matrix, Vector]:
+def make_mc(values: MatrixType | list[float]) -> Matrix | Vector:
+    """Creates a :class:`Matrix` or a :class:`Vector` object depending on input values type.
+
+    Example:
+
+    >>> a: Matrix = make_matrix([
+    ...     [1, 2],
+    ...     [3, 4]])
+    >>> b: Vector = make_vector([1, 2])
+    """
+
+    if isinstance(values[0], list):
+        return make_matrix(values)
+    return make_vector(values)
